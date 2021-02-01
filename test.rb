80ffe00d9c12c27657e7c5e91e6dda70a186dd70
Richard Adule <big.richi@outlook.com>
2021-02-01 23:10:22 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,2 +1,28 @@
-x = "hello world"
-p x
+require "pry"
+
+class Housemate
+
+    def pay_rent(number)
+        binding.pry
+        "#{self} pays $#{number} in rent today."
+    end
+
+    def clean
+        "It's time for #{self} to clean the apartment."
+    end
+
+    def moving_out(fake_address)
+        "#{self} is moving to #{fake_address}."
+    end
+
+    def chicken
+        "Hey #{self} watch out ther is a chicken in the house!"
+    end
+
+end
+
+james = Housemate.new
+
+"Richard" = Housemate.new
+
+"Richard".pay_rent(300)
